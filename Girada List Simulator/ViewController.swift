0038bc71c153b5d76303bee3b9206307c9947aa8
Luigi De Marco <ldm1294apple@gmail.com>
2017-10-27 20:31:27 +0200

    Initial Commit

---
+++ Girada List Simulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Girada List Simulator
+//
+//  Created by Luigi De Marco on 27/10/17.
+//  Copyright © 2017 Luigi De Marco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Girada List SimulatorTests/Girada_List_SimulatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Girada_List_SimulatorTests.swift
+//  Girada List SimulatorTests
+//
+//  Created by Luigi De Marco on 27/10/17.
+//  Copyright © 2017 Luigi De Marco. All rights reserved.
+//
+
+import XCTest
+@testable import Girada_List_Simulator
+
+class Girada_List_SimulatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
